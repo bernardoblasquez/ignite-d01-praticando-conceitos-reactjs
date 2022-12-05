fd46a416592dc5b06181d5b780a0de3a50d2ef76
Bernardo <bernardo.bt@gmail.com>
2022-12-05 08:17:41 -0300

    refactor: mudança na chamada do componente e correção do estilo global.

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react' 
 import { Header } from './components/Header'
+import { ToDoApp } from './components/ToDoApp'
 import './global.scss'
 
 export function App() {
 
   return (
-    <Header />
+    <ToDoApp />
   )
 }
