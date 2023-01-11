980d5a623c8726a87896d731f70148741b4b4100
Bernardo <bernardo.bt@gmail.com>
2023-01-11 07:45:13 -0300

    feat: implementação da interface de dados recebidos pelo app

--- todo-list/src/components/ToDoApp.tsx
+++ todo-list/src/components/ToDoApp.tsx
@@ -1,11 +1,34 @@
+import id from "date-fns/esm/locale/id/index.js";
 import { Header } from "./Header";
 import { TaskList } from "./TaskList";
+import { EmptyList } from "./EmptyList";
+import { ListIndicators } from "./ListIndicators";
 
-export function ToDoApp(){
+
+interface Task {
+   description: string,
+   isDone: boolean
+}
+interface ToDoList{
+   id: number,
+   task: Task
+}
+
+interface ToDoListProps{
+   listOfTasks: ToDoList[]
+}
+
+
+
+export function ToDoApp({listOfTasks}:ToDoListProps){
+
+   console.log(listOfTasks)
+
    return(
       <>
          <Header />
-
+         <ListIndicators />
+         {/*<EmptyList />*/}
          <TaskList />
       </>
    )
