b991d2cccc1210563833b6bab6f0f05e6993a2b4
Bernardo <bernardo.bt@gmail.com>
2023-01-11 07:43:47 -0300

    feat: estrutura dos elementos da lista de tarefas.

--- todo-list/src/components/TaskList.tsx
+++ todo-list/src/components/TaskList.tsx
@@ -1,31 +1,27 @@
-import clipboard from '../assets/Clipboard.svg'
+import {Trash, Circle, CheckCircle} from 'phosphor-react'
+import checkedIcon from '../assets/Checked.svg'
+import checkIcon from '../assets/Check.svg'
 import style from './tasklist.module.scss'
 import './tasklist.module.scss' /* bug fix  https://github.com/vitejs/vite/issues/6907*/
 
 export function TaskList(){
    return(
-      <div className={style["task-list"]}>
-         <div className={style["task-list__indicators"]}>
-            <span className={style["task-list__indicator"]}>
-               <span className={style["task-list__indicator-label"]}>Tarefas Criadas</span> 
-               <span className={style["task-list__indicator-number"]}>0</span>
-            </span>
-
-            <span className={style["task-list__indicator"]}>
-               <span className={style["task-list__indicator-label"]}>Concluídas</span>
-               <span className={style["task-list__indicator-number"]}>0</span>
-            </span>
-         </div>
-
-         <div className={style["task-list__empty-list"]}>
-            <img src={clipboard} alt="" />
-            <p>
-               <b>Você ainda não tem tarefas cadastradas</b>
+      <ul className={style["task-list"]}>
+         <li className={style["task-item"]}> 
+            <img src={checkedIcon} alt="" />
+            <p className={style["checked-text"]}>
+               Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
             </p>
+            <Trash size={24} weight="bold" />
+         </li>
+
+         <li className={style["task-item"]}> 
+            <img src={checkIcon} alt="" />
             <p>
-               Crie tarefas e organize seus itens a fazer
+               Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
             </p>
-        </div>
-      </div>
+            <Trash size={24} weight="bold" />
+         </li>
+      </ul>
    )
 }
