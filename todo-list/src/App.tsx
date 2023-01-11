15258d18cd0a1d90dac440ac8b8638773d00e6b2
Bernardo <bernardo.bt@gmail.com>
2023-01-11 07:37:21 -0300

    feat: modelo da dados (json) que o app vai receber

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -3,9 +3,52 @@ import { Header } from './components/Header'
 import { ToDoApp } from './components/ToDoApp'
 import './global.scss'
 
+const toDoList = [
+  {
+    id: 1,
+    task: {
+      description: 'Estudar Projeto 01 do Ignite',
+      isDone: true
+    }
+  },
+
+  {
+    id: 2,
+    task: {
+      description: 'Estudar Projeto 02 do Ignite',
+      isDone: false
+    }
+  },
+
+  {
+    id: 3,
+    task: {
+      description: 'Estudar Fundos imobiliários - MNC1',
+      isDone: false
+    }
+  },
+
+  {
+    id: 4,
+    task: {
+      description: 'Estudar derivativos e estruturas - MNC1',
+      isDone: false
+    }
+  },
+
+  {
+    id: 5,
+    task: {
+      description: 'Estudar Scrum',
+      isDone: true
+    }
+  },
+];
+
+
 export function App() {
 
   return (
-    <ToDoApp />
+    <ToDoApp listOfTasks={toDoList} />
   )
 }
